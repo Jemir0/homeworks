eeb738538a6ea60a359100201566079c27977d9f
Jemir0 <sanyabog3@gmail.com>
2022-09-29 18:34:57 +0300

    fix math issues

--- JS_1/src/js/app.js
+++ JS_1/src/js/app.js
@@ -11,12 +11,12 @@ alert('Ваш вік - ' + userAge + '.');
 
 // Third task
 const perimeter = prompt('3. Вкажіть довжину сторони квадрату (в сантиметрах), щоб дізнатись його периметр.');
-alert('Периметр квадрата - ' + perimeter * 2 + ' см.');
+alert('Периметр квадрата - ' + perimeter * 4 + ' см.');
 
 // Maximum
 // Fourth task
 const radius = prompt('4. Вкажіть радіус кола (в сантиметрах), щоб дізнатись його площу.');
-alert('Площа кола ' + Math.PI * Math.pow(radius, 2) + ' см^2.');
+alert('Площа кола ' + (Math.PI * Math.pow(radius, 2)).toFixed(2) + ' см^2.');
 
 //Fifth task
 const distance = prompt('5.1 Вкажіть відстань між містами (в кілометрах).');
